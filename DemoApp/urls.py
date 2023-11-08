80d936e528e1f9fa73d11fd45f90315a966f58c4
Prasad <prasadprabhudesai11@gmail.com>
2023-11-08 09:55:02 -0800

    Updating URL file due to Django 4 Update

--- DemoApp/urls.py
+++ DemoApp/urls.py
@@ -1,10 +1,10 @@
-from django.conf.urls import url
+from django.urls import re_path
 from django.contrib import admin
 from . import views
 from django.conf import settings
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^$', views.index),
-    url(r'trial$', views.trial)
+    re_path(r'^admin/', admin.site.urls),
+    re_path(r'^$', views.index),
+    re_path(r'trial$', views.trial)
 ]
